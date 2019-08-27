2c49bb84b2db269eb93fe1d87c7f48dd91908d1b
UcheSylvester <okorocode@gmail.com>
2019-08-27 19:28:33 +0100

    users component done!

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "github-users",
+  templateUrl: "./users.component.html"
+})
+export class UsersComponent {
+  // pageTile: string =
+}
