4c18e02ab562a14a1e3d1c3790978bc556af991d
UcheSylvester <okorocode@gmail.com>
2019-08-27 22:25:24 +0100

    dummy files for users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -5,5 +5,38 @@ import { Component } from "@angular/core";
   templateUrl: "./users.component.html"
 })
 export class UsersComponent {
-  // pageTile: string =
+  persons: any[] = [
+    {
+      userId: 1,
+      fullName: "Leaf Rake",
+      userName: "UcheSylvester",
+      following: 13,
+      followers: 14,
+      imageUrl: "assets/images/uc.JPG"
+    },
+    {
+      userId: 2,
+      fullName: "Garden Cart",
+      userName: "UcheSylvester",
+      following: 13,
+      followers: 14,
+      imageUrl: "assets/images/uc.JPG"
+    },
+    {
+      userId: 5,
+      fullName: "Hammer",
+      userName: "UcheSylvester",
+      following: 13,
+      followers: 14,
+      imageUrl: "assets/images/uc.JPG"
+    },
+    {
+      userId: 8,
+      fullName: "Saw",
+      userName: "UcheSylvester",
+      following: 13,
+      followers: 14,
+      imageUrl: "assets/images/uc.JPG"
+    }
+  ];
 }
